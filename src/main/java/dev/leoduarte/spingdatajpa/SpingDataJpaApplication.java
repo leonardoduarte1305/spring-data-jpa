4af4d28fe60bf2224d198599ea8f3d4290f7d9a5
LeoDuarte <leonardo.duarte@maps.com.br>
2024-06-25 09:30:25 -0300

    First Commit!

---
+++ src/main/java/dev/leoduarte/spingdatajpa/SpingDataJpaApplication.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.spingdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/leoduarte/spingdatajpa/SpingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.spingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
