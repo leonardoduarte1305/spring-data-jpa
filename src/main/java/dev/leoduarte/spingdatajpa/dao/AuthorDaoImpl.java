656c25cc5417c34aafaa27a4acc945add8f08add
LeoDuarte <leonardo.duarte@maps.com.br>
2024-07-06 09:11:54 -0300

    JDBC - DAO PATTERN 6 - Implementation
    Author getById(long id);
    Author getByFirstNameAndLastName(String firstName, String lastName);
    Author saveNewAuthor(Author Author);
    Author updateAuthor(Long id, Author Author);
    void deleteById(Long id);

---
+++ src/main/java/dev/leoduarte/spingdatajpa/dao/AuthorDaoImpl.java
@@ -0,0 +1,184 @@
+package dev.leoduarte.spingdatajpa.dao;
+
+import dev.leoduarte.spingdatajpa.domain.Author;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource source;
+
+    @Override
+    public Author getById(long id) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM author WHERE author.id = ?");
+            ps.setLong(1, id);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+                return buildAuthorToReturn(id, resultSet);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Entity not found with id = " + id, e);
+        } finally {
+            closeAllResources(resultSet, ps, connection);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Author getByFirstNameAndLastName(String firstName, String lastName) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM author WHERE first_name = ? AND last_name = ?");
+            ps.setString(1, firstName);
+            ps.setString(2, lastName);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+                return buildAuthorToReturn(resultSet.getLong("id"), resultSet);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Entity not found with firstName = " + firstName + " and lastName = " + lastName, e);
+        } finally {
+            closeAllResources(resultSet, ps, connection);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Author saveNewAuthor(Author Author) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+            long idToBeSaved = getTheNextAuthorId(connection);
+
+            ps = connection.prepareStatement("INSERT INTO author (id, first_name, last_name) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, idToBeSaved);
+            ps.setString(2, Author.getFirstName());
+            ps.setString(3, Author.getLastName());
+            ps.execute();
+
+            return getById(idToBeSaved);
+        } catch (SQLException e) {
+            throw new RuntimeException("The entity could not be saved properly.", e);
+        } finally {
+            closeAllResources(resultSet, ps, connection);
+        }
+    }
+
+    @Override
+    public Author updateAuthor(Long id, Author Author) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+
+            ps = connection.prepareStatement("UPDATE author SET author.first_name = ?, author.last_name = ? WHERE author.id = ?", Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, Author.getFirstName());
+            ps.setString(2, Author.getLastName());
+            ps.setLong(3, id);
+            ps.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("The entity could not be saved properly.", e);
+        } finally {
+            closeAllResources(resultSet, ps, connection);
+        }
+
+        return getById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+
+            ps = connection.prepareStatement("DELETE FROM author WHERE author.id = ?", Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, id);
+            ps.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("The entity could not be deleted properly.", e);
+        } finally {
+            closeAllResources(null, ps, connection);
+        }
+    }
+
+    private long getTheNextAuthorId(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT next_val FROM author_seq");
+        if (resultSet.next()) {
+            long nextId = resultSet.getLong(1);
+            statement.executeUpdate("UPDATE author_seq SET next_val = " + (nextId + 1));
+
+            statement.close();
+            resultSet.close();
+            return nextId;
+        } else {
+            statement.close();
+            resultSet.close();
+            throw new SQLException("Failed to retrieve next sequence value.");
+        }
+    }
+
+    private static Author buildAuthorToReturn(long id, ResultSet resultSet) throws SQLException {
+        return new Author(id, resultSet.getString("first_Name"), resultSet.getString("last_Name"));
+    }
+
+    private static void closeAllResources(ResultSet resultSet, PreparedStatement ps, Connection connection) {
+        try {
+            closeResultSet(resultSet);
+            closePreparedStatement(ps);
+            closeConnection(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error closing one or more of those resources.", e);
+        }
+    }
+
+    private static void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
+
+    private static void closePreparedStatement(PreparedStatement ps) throws SQLException {
+        if (ps != null) {
+            ps.close();
+        }
+    }
+
+    private static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
---
+++ src/test/java/dev/leoduarte/spingdatajpa/dao/AuthorDaoImplIntegrationTest.java
@@ -0,0 +1,102 @@
+package dev.leoduarte.spingdatajpa.dao;
+
+import dev.leoduarte.spingdatajpa.domain.Author;
+import dev.leoduarte.spingdatajpa.repository.AuthorRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@DataJpaTest
+@ComponentScan(basePackages = {"dev.leoduarte.spingdatajpa.dao"})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("local")
+class AuthorDaoImplIntegrationTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Autowired // Just to get the id, because it is dinamically generated
+    AuthorRepository authorRepository;
+
+    @Test
+    @Order(1)
+    void saveAuthor() {
+        String firstName = "First Name";
+        String lastName = "Last Name";
+        Author authorToSave = new Author(null, firstName, lastName);
+        Author author = authorDao.saveNewAuthor(authorToSave);
+
+        Assertions.assertThat(author).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    void getById() {
+        String firstName = "First Name";
+        String lastName = "Last Name";
+        Author authorToSave = new Author(null, firstName, lastName);
+
+        Author author = authorDao.saveNewAuthor(authorToSave);
+        Assertions.assertThat(author).isNotNull();
+
+        Author foundAuthor = authorDao.getById(author.getId());
+
+        Assertions.assertThat(foundAuthor.getFirstName()).isEqualTo(firstName);
+        Assertions.assertThat(foundAuthor.getLastName()).isEqualTo(lastName);
+    }
+
+    @Test
+    @Order(3)
+    void getAuthorByFirstNameAndLastName() {
+        String firstName = "First Name";
+        String lastName = "Last Name";
+        Author book = authorDao.getByFirstNameAndLastName(firstName, lastName);
+
+        Assertions.assertThat(book).isNotNull();
+    }
+
+    @Test
+    @Order(4)
+    void updateAuthor() {
+        Author authorToSave = getNewAuthor();
+        Author authorAlreadySaved = authorDao.saveNewAuthor(authorToSave);
+
+        Assertions.assertThat(authorAlreadySaved).isNotNull();
+
+        String firstName = "Completely FirstName";
+        authorAlreadySaved.setFirstName(firstName);
+        Author authorUpdated = authorDao.updateAuthor(authorAlreadySaved.getId(), authorAlreadySaved);
+
+        Assertions.assertThat(authorUpdated.getFirstName()).isEqualTo(firstName);
+    }
+
+    @Test
+    @Order(5)
+    void deleteAuthorById() {
+        Author authorToSave = getNewAuthor();
+        Author savedAuthor = authorDao.saveNewAuthor(authorToSave);
+
+        Assertions.assertThat(savedAuthor).isNotNull();
+
+        authorDao.deleteById(savedAuthor.getId());
+
+        Author deletedAuthor = authorDao.getById(savedAuthor.getId());
+
+        Assertions.assertThat(deletedAuthor).isNull();
+    }
+
+    private static Author getNewAuthor() {
+        String firstName = "First Name";
+        String lastName = "Last Name";
+        return new Author(null, firstName, lastName);
+    }
+}
