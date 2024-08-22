6cb8c2e700280cc2a9c586a52daa43c18f5072f9
LeoDuarte <leonardo.duarte@maps.com.br>
2024-08-22 15:11:42 -0300

    CALLBACKS - Removing Listeners to use CALLBACK

--- src/main/java/dev/leoduarte/spingdatajpa/domain/listeners/config/ListenerRegistration.java
+++ src/main/java/dev/leoduarte/spingdatajpa/domain/listeners/config/ListenerRegistration.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class ListenerRegistration implements BeanPostProcessor {
 
@@ -27,15 +27,15 @@ public class ListenerRegistration implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof LocalContainerEntityManagerFactoryBean) {
-            LocalContainerEntityManagerFactoryBean lemf = (LocalContainerEntityManagerFactoryBean) bean;
-            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
-            EventListenerRegistry registry = sessionFactory.getServiceRegistry().getService(EventListenerRegistry.class);
-
-            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
-            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
-            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
-        }
+//        if (bean instanceof LocalContainerEntityManagerFactoryBean) {
+//            LocalContainerEntityManagerFactoryBean lemf = (LocalContainerEntityManagerFactoryBean) bean;
+//            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
+//            EventListenerRegistry registry = sessionFactory.getServiceRegistry().getService(EventListenerRegistry.class);
+//
+//            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
+//            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
+//            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
+//        }
         return bean;
     }
 }
